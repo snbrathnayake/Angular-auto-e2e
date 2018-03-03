af2be664475e61b3869e85c612ab8cbd85b39aab
snbrathnayake <snbrathnayake@gmail.com>
2018-03-03 20:57:18 +0530

    hello

--- e2e/config/routes.e2e.ts
+++ e2e/config/routes.e2e.ts
@@ -1,15 +1,11 @@
 
+
 // urlPaths
 export const PathURL = {
-  
-  u1: 'https://apple.com/',
+
   home: 'http://localhost:4200/#/home',
-  log: 'http://localhost:4200/#/orders',
   about: 'http://localhost:4200/#/about',
   loginURL: 'https://was.malachite.veracode.com/login/#!/login',
   baseURL: 'https://was.malachite.veracode.com/mvsa/#',
-  facebook: 'https://facebook.com/',
-
-  u2: 'https://google.com/',
-
-};
+  youtube: 'https://www.youtube.com/',
+};  
