a510ccb7a9d76559b2b1e18c2b865466b1df3711
snbrathnayake <snbrathnayake@gmail.com>
2018-03-03 21:36:11 +0530

    hell3

--- e2e/config/routes.e2e.ts
+++ e2e/config/routes.e2e.ts
@@ -3,10 +3,13 @@
 // urlPaths
 export const PathURL = {
 
+  u1: 'https://apple.com/',
   home: 'http://localhost:4200/#/view',
-  home: 'http://localhost:4200/#/login',
+  login: 'http://localhost:4200/#/login',
   about: 'http://localhost:4200/#/about',
   loginURL: 'https://was.malachite.veracode.com/login/#!/login',
   baseURL: 'https://was.malachite.veracode.com/mvsa/#',
   youtube: 'https://www.youtube.com/v1',
+
+  u2: 'https://google.com/',
 }
