ff49e744df700b512beb4998465da66f9b2f3f67
Sameera Rathnayake <snbrathnayake@gmail.com>
2018-03-03 20:09:15 +0530

    Update routes.e2e.ts

--- e2e/config/routes.e2e.ts
+++ e2e/config/routes.e2e.ts
@@ -1,7 +1,7 @@
 
 // urlPaths
 export const PathURL = {
-  home: 'http://localhost:4200/#/blog',
+  home: 'http://localhost:4200/#/home',
   about: 'http://localhost:4200/#/about',
   loginURL: 'https://was.malachite.veracode.com/login/#!/login',
   baseURL: 'https://was.malachite.veracode.com/mvsa/#',
